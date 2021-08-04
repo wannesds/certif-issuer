36a3d65dbe1c4b3d882457e35cc4e08a7759dc36
Wannes Dieltiens <50752071+wannesds@users.noreply.github.com>
2021-08-04 16:05:31 +0200

    fixed acl issue

--- src/utils/addReadAccess.js
+++ src/utils/addReadAccess.js
@@ -6,44 +6,66 @@ import {
     hasAcl,
     getThing,
     getUrlAll,
-    getSolidDataset
+    getSolidDataset,
+    hasResourceAcl,
+    createAclFromFallbackAcl
   } from "@inrupt/solid-client";
 
   
-export async function AddReadAccess(certifListStored, session, userId){
+export async function AddReadAccess(session, userId){
     const STORAGE_PREDICATE = "http://www.w3.org/ns/pim/space#storage";
 
     try {
         const profileDataset = await getSolidDataset(session.info.webId, {
             fetch: session.fetch,
         });
-          const profileThing = getThing(profileDataset, session.info.webId);
-          const podsUrls = getUrlAll(profileThing, STORAGE_PREDICATE);
-          const pod = podsUrls[0];
-          const containerUri = `${pod}certificates-issued/`
+        const profileThing = getThing(profileDataset, session.info.webId);
+        const podsUrls = getUrlAll(profileThing, STORAGE_PREDICATE);
+        const pod = podsUrls[0];
+        const containerUri = `${pod}certificates-issued/index.ttl`
         //had to do these steps to get correct link, might be able to get it from session tho
         
-        console.log('urltest', containerUri, certifListStored)
-        // Fetch the SolidDataset and its associated ACLs, if available:
-        const myDatasetWithAcl = await getSolidDatasetWithAcl(containerUri, {
+        const myDataset = await getSolidDatasetWithAcl(containerUri, {
             fetch: session.fetch
-        });
-        console.log("myDatasetWithAcl :", myDatasetWithAcl)
-
-        const resourceAcl = getResourceAcl(myDatasetWithAcl);
-        //console.log("resourceAcl", resourceAcl)
-        console.log("hasAcl?", hasAcl(myDatasetWithAcl))
-        console.log("userId", userId)
-        console.log("session", session)
+        }); 
+        let resourceAcl = "";
+        if (hasResourceAcl(myDataset)) {
+            resourceAcl = getResourceAcl(myDataset)
+        } else {
+            resourceAcl = createAclFromFallbackAcl(myDataset)
+        }
+        
         const updatedAcl = setAgentResourceAccess(
             resourceAcl,
             userId,
             { read: true, append: false, write: false, control: false }
         );
         console.log("updatedAcl : ", updatedAcl)
-        await saveAclFor(myDatasetWithAcl, updatedAcl , {
+        await saveAclFor(myDataset, updatedAcl , {
             fetch: session.fetch
         });
+        /////// VVVVV to be deleted VVVVV
+        // console.log('urltest', containerUri, certifListStored)
+        // // Fetch the SolidDataset and its associated ACLs, if available:
+        // const myDatasetWithAcl = await getSolidDatasetWithAcl(containerUri, {
+        //     fetch: session.fetch
+        // });
+        // console.log("myDatasetWithAcl :", myDatasetWithAcl)
+
+        // const resourceAcl = getResourceAcl(myDatasetWithAcl);
+        // //console.log("resourceAcl", resourceAcl)
+        // console.log("hasAcl?", hasAcl(myDatasetWithAcl))
+        // console.log("userId", userId)
+        // console.log("session", session)
+        // const updatedAcl = setAgentResourceAccess(
+        //     resourceAcl,
+        //     userId,
+        //     { read: true, append: false, write: false, control: false }
+        // );
+        // console.log("updatedAcl : ", updatedAcl)
+        // await saveAclFor(myDatasetWithAcl, updatedAcl , {
+        //     fetch: session.fetch
+        // });
 
        
     } catch {
